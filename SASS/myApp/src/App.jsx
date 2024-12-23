591df56c5803395f0e77f99e29334477a4cc099b
VinaySharmaQss <vinay.sharma@qsstechnosoft.com>
2024-12-23 18:27:36 +0530

    First Commit

---
+++ SASS/myApp/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './styles/app.scss'
+
+function App() {
+  return (
+    <>
+      <div>
+        <button>Hello World</button>
+        Hello World !
+        </div>
+        <footer>
+        <div>Button</div>
+        </footer>
+        
+    </>
+  )
+}
+
+export default App
